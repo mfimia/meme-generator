ee92081328842b4f16747020f7092c271a1e7c8e
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-25 09:26:56 +0100

    added meme data array

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,22 @@
 export default function Meme() {
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.log("I was clicked!");
+  };
+
   return (
     <form className="meme--form">
       <div className="form--input--text--container">
-        <input className="form--text--1" type="text" />
-        <input className="form--text--2" type="text" />
+        <input className="form--text--1" placeholder="Top text" type="text" />
+        <input
+          className="form--text--2"
+          placeholder="Bottom text"
+          type="text"
+        />
       </div>
-      <input
-        className="form--submit"
-        value="Get a new meme image 👀"
-        type="button"
-      />
+      <button onClick={handleClick} className="form--submit">
+        Get a new meme image 👀
+      </button>
     </form>
   );
 }
