2271c078262541f9dfd974d7548349aac5306a22
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-25 14:38:38 +0100

    randomized meme url via usestate

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,26 +1,32 @@
 import memes from "../memesData";
+import { useState } from "react";
 
+console.log(memes);
 export default function Meme() {
+  let randomNumber = Math.floor(Math.random() * memes.length);
+  const [meme, setMeme] = useState(memes[randomNumber]);
   const getMemeImage = (event) => {
     event.preventDefault();
-    const randomNumber = Math.floor(Math.random() * memes.length);
-    const chosenMeme = memes[randomNumber];
-    console.log(chosenMeme);
+    randomNumber = Math.floor(Math.random() * memes.length);
+    setMeme(memes[randomNumber]);
   };
 
   return (
-    <form className="meme--form">
-      <div className="form--input--text--container">
-        <input className="form--text--1" placeholder="Top text" type="text" />
-        <input
-          className="form--text--2"
-          placeholder="Bottom text"
-          type="text"
-        />
-      </div>
-      <button onClick={getMemeImage} className="form--submit">
-        Get a new meme image 👀
-      </button>
-    </form>
+    <main>
+      <form className="meme--form">
+        <div className="form--input--text--container">
+          <input className="form--text--1" placeholder="Top text" type="text" />
+          <input
+            className="form--text--2"
+            placeholder="Bottom text"
+            type="text"
+          />
+        </div>
+        <button onClick={getMemeImage} className="form--submit">
+          Get a new meme image 👀
+        </button>
+      </form>
+      <img src={meme.url} className="meme--img" alt="Random meme" />
+    </main>
   );
 }
