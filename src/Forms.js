38e79609facf382c1c11895032de29175481b410
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-11-26 16:53:35 +0100

    added radio button to form page

--- src/Forms.js
+++ src/Forms.js
@@ -8,6 +8,7 @@ export default function Form() {
     email: "",
     comments: "",
     isFriendly: true,
+    emplyment: "",
   });
 
   //   We handle all inputs with the same eventhandler. Registering everysingle state change
@@ -68,6 +69,43 @@ export default function Form() {
       {/* We link the label via "htmlFor" */}
       <label htmlFor="isFriendly">Are you friendly?</label>
       <br />
+
+      <fieldset>
+        <legend>Current employment status</legend>
+
+        <input
+          type="radio"
+          id="unemployed"
+          name="employment"
+          value="unemployed"
+          checked={formData.employment === "unemployed"}
+          onChange={handleChange}
+        />
+        <label htmlFor="unemployed">Unemployed</label>
+        <br />
+
+        <input
+          type="radio"
+          id="part-time"
+          name="employment"
+          value="part-time"
+          checked={formData.employment === "part-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="part-time">Part-time</label>
+        <br />
+
+        <input
+          type="radio"
+          id="full-time"
+          name="employment"
+          value="full-time"
+          checked={formData.employment === "full-time"}
+          onChange={handleChange}
+        />
+        <label htmlFor="full-time">Full-time</label>
+        <br />
+      </fieldset>
     </form>
   );
 }
